88a9bc5534dde6d2fa2b0d6a8984b43604283bb5
Danish Hussain <danishhussain.jmi@gmail.com>
2024-03-15 01:25:57 +0530

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodeJS,not browser
+
+console.log(3 
+    + 
+    3);    // code readability should be high
+
+
+
+console.log("Danish")
+
+
+let name = "Danish"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+//  boolean => true/false
+// null => standalone value
+// ubdefined
+//  symbol => unique
+
+
+// object
+
+console.log(typeof null )
+// console.log([typeof] Danish)
+
+
+

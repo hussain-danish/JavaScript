496f741714a13a1f86ecf4723899353964d56f73
Danish Hussain <danishhussain.jmi@gmail.com>
2024-03-15 15:08:12 +0530

    conversionOperationConfusionClear

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33sd"
+// let score = null
+let score = undefined 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33sd" => NaN (Not a number)
+// true => 1;
+// false => 0;
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Danish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true: 0 => false
+// "" => false
+// "Danish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

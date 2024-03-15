9c21d67d5e48a2530d772d93d8999b71e0b22f15
Danish Hussain <danishhussain.jmi@gmail.com>
2024-03-15 16:30:57 +0530

    Comparisions

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -12,8 +12,7 @@
 // console.log(null == 0);
 // console.log(null >= 0);
 
-// The reason is that an equality check == and comparisions > < >= <= work differently
-// Comparisions convert null to a Number, treating it as 0. That's why (3) null>=0 is true and (1)null >0 is false.
+// The reason is that an equality check == and comparisions > < >= <= work differently Comparisions convert null to a Number, treating it as 0. That's why (3) null>=0 is true and (1)null >0 is false.
 
 // console.log(undefined == 0);
 // console.log(undefined > 0);

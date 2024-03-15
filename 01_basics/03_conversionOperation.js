c1c4f4d1cb3dd8e97e63577976acfc66ff6014b7
Danish Hussain <danishhussain.jmi@gmail.com>
2024-03-15 16:01:13 +0530

    conversionOperationConfusionClear

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,3 +29,34 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Danish"
+let str3 = str1+ str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 +"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// Link to Study
+// https://tc39.es/ecma262/#sec-abstract-operations
